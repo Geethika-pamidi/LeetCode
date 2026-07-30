819490841881d477dfe2bf66dc6b13a9df72ef4e
PAMIDI LAKSHMI GEETHIKA <149164363+Geethika-pamidi@users.noreply.github.com>
2026-07-30 10:39:30 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.32%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        while(n !=1 && n !=4){
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                sum+=digit*digit;
+                n/=10;
+            }
+            n=sum;
+        }
+        return n==1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
