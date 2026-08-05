e3f6c0ab072fd83519536a2bc3f3a79ac16e8ae4
PAMIDI LAKSHMI GEETHIKA <149164363+Geethika-pamidi@users.noreply.github.com>
2026-08-05 14:49:39 +0530

    Time: 0 ms (100%), Space: 8 MB (31.95%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        return (n % 2 == 0) ? n : 2 * n;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
